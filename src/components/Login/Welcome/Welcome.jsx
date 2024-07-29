fc2d2027f14f8e84f878a241080faf38b150a692
RJRYT <robinjratr@gmail.com>
2024-07-30 03:43:52 +0530

    image fix

--- src/components/Login/Welcome/Welcome.jsx
+++ src/components/Login/Welcome/Welcome.jsx
@@ -4,24 +4,25 @@ import { Link } from 'react-router-dom';
 import DemoImage from "../../../assets/login/image1.jpg";
 import Phone from "../../../assets/login/phone.png";
 import Google from "../../../assets/login/google.png";
+import Illustrtion from "../../../assets/login/illustration.jpg";
 
 function Welcome() {
   return (
     <div className={Styles.welcomeContiner}>
-      <div className={Styles.circleContainer}>
-        <div className={`${Styles.circle} ${Styles.circleOuter}`}></div>
-        <div className={`${Styles.circle} ${Styles.circleInner}`}></div>
-        <div className={`${Styles.circle} ${Styles.circleCenter}`}>
-          <img src={DemoImage} alt="center image1" />
+      {/* <div className={Styles.circleContainer}> */}
+        {/* <div className={`${Styles.circle} ${Styles.circleOuter}`}></div>
+        <div className={`${Styles.circle} ${Styles.circleInner}`}></div> */}
+        <div className={Styles.imageContainer}>
+          <img src={Illustrtion} alt="center image1" />
         </div>
-        <img src={DemoImage} className={Styles.image} alt="image1" />
+        {/* <img src={DemoImage} className={Styles.image} alt="image1" />
         <img src={DemoImage} className={Styles.image} alt="image2" />
         <img src={DemoImage} className={Styles.image} alt="image3" />
         <img src={DemoImage} className={Styles.image} alt="image4" />
         <img src={DemoImage} className={Styles.image} alt="image5" />
         <img src={DemoImage} className={Styles.image} alt="image6" />
-        <img src={DemoImage} className={Styles.image} alt="image7" />
-    </div>
+        <img src={DemoImage} className={Styles.image} alt="image7" /> */}
+    {/* </div> */}
     <div className={Styles.text}>Let’s meeting new <br/> people around you</div>
     <div className={Styles.buttonContainer}>
         <Link href="#" className={Styles.button}><img src={Phone} alt="Phone Icon" /> Login with Phone</Link>
