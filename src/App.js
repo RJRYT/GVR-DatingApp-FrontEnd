aa77451e4a9de24be8360ffa21bcc155876dd27e
RJRYT <robinjratr@gmail.com>
2024-07-29 15:22:26 +0530

    browser routing added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginWelcome from "./components/Login/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="page-content">
+      <main className='container'>
+        <Suspense fallback={<div>Loading...</div>}>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Navigate to="/login" />} />
+              <Route path='/login' element={<LoginWelcome />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
+      </main>
+    </section>
   );
 }
 
