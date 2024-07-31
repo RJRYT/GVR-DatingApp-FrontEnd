70b8c143387730d27548faf432f3b21806020b6d
RJRYT <robinjratr@gmail.com>
2024-07-31 20:17:56 +0530

    authContext & Custom axios instance added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
+import "react-toastify/dist/ReactToastify.min.css";
 import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./contexts/AuthContext";
 import LoginWelcome from "./components/Login/Welcome";
 
 function App() {
   return (
-    <section className="page-content font-sans">
-      <main className='container mx-auto p-4 max-w-[600px]'>
-        <Suspense fallback={<div>Loading...</div>}>
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Navigate to="/login" />} />
-              <Route path='/login' element={<LoginWelcome />} />
-            </Routes>
-          </BrowserRouter>
-        </Suspense>
-      </main>
-    </section>
+    <AuthProvider>
+      <ToastContainer newestOnTop={true} theme="colored" />
+      <section className="page-content font-sans">
+        <main className='container mx-auto p-4 max-w-[600px]'>
+          <Suspense fallback={<div>Loading...</div>}>
+            <BrowserRouter>
+              <Routes>
+                <Route path='/' element={<Navigate to="/login" />} />
+                <Route path='/login' element={<LoginWelcome />} />
+              </Routes>
+            </BrowserRouter>
+          </Suspense>
+        </main>
+      </section>
+    </AuthProvider>
   );
 }
 
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,45 @@
+import React, { createContext, useState, useEffect, useCallback } from "react";
+import axiosInstance from "../Instance/Axios";
+import { toast } from "react-toastify";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [authState, setAuthState] = useState({
+    isAuthenticated: false,
+    user: null,
+  });
+  const [loading, setLoading] = useState(true);
+
+  const checkAuthStatus = useCallback(async () => {
+    try {
+      const response = await axiosInstance.get("/users/me");
+      setAuthState({ isAuthenticated: true, user: response.data });
+    } catch (error) {
+      setAuthState({ isAuthenticated: false, user: null });
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, [checkAuthStatus]);
+
+  const logout = async () => {
+    try {
+      setAuthState({ isAuthenticated: false, user: null });
+      await axiosInstance.post("/auth/logout");
+      toast.warning("Logout completed");
+      window.history.location = "/login";
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ authState, checkAuthStatus, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
