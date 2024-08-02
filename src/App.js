eaf4df7b3ff40eb632c23ec445d43dc738197633
sooryaram89 <sooryaram.89@gmail.com>
2024-08-02 14:52:32 +0530

    ui changes tabs

--- src/App.js
+++ src/App.js
@@ -12,19 +12,20 @@ import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <section className="page-content font-sans">
-      <main className='container mx-auto p-4 max-w-[600px]'>
-        <Suspense fallback={<div>Loading...</div>}>
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Navigate to="/login" />} />
-              <Route path='/login' element={<LoginWelcome />} />
-
-            </Routes>
-          </BrowserRouter>
-        </Suspense>
-      </main>
-    </section>
+    <Suspense fallback={<Loading />}>
+      <AuthProvider>
+        <ToastContainer newestOnTop={true} theme="colored" />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/" element={<DefaultPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </Suspense>
   );
 }
 
