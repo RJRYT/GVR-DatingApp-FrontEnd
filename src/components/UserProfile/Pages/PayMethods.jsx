8777501df4cc9760b2bd129259298e4ad406f8d5
RJRYT <robinjratr@gmail.com>
2024-08-13 09:53:47 +0530

    paymethods & add card style updated

--- src/components/UserProfile/Pages/PayMethods.jsx
+++ src/components/UserProfile/Pages/PayMethods.jsx
@@ -10,23 +10,23 @@ import { IoIosArrowDropdownCircle } from "react-icons/io";
 const PayMethods = () => {
   return (
     <>
-      <div className="relative bg-white rounded-t-3xl min-h-screen p-6 mt-4">
-        <div className="flex items-center justify-between absolute top-5 left-0 right-0 px-6">
+      <div className="relative bg-white rounded-t-3xl min-h-screen mt-4 space-y-6">
+        <div className="flex items-center justify-between px-6 py-8 md:mx-16">
           <RiCloseLine className="text-black text-3xl cursor-pointer" />
+          <h1 className=" text-fuchsia-900 text-2xl font-medium text-center mx-auto tracking-wide">
+            Payments methods
+          </h1>
           <span className="text-black font-mono text-xl">Done</span>
         </div>
 
-        <h1 className="mt-10 text-fuchsia-900 text-2xl font-medium text-center w-full max-w-screen mx-auto pt-3 pb-4 tracking-wide">
-          Payments methods
-        </h1>
-        <p className="text-left text-xs font-semibold ml-3 mr-1">
+        <p className="text-left text-xs font-semibold ml-3 mr-1 px-3 md:mx-16">
           choose desired payment type.We offer easy ways
           <br /> for payments on our app
         </p>
-        <div className="mt-4 mx-3 p-4 border border-red-600 rounded-md flex items-center justify-between">
+        <div className="mt-4 mx-3 md:mx-16 p-4 border border-red-600 rounded-md flex items-center justify-between">
           <img
             src={MasterCard}
-            className="w-[25%] mr-4"
+            className="w-20 aspect-[3/2] object-contain"
             alt="MasterCard Logo"
           />
           <div className="text-left">
@@ -39,10 +39,10 @@ const PayMethods = () => {
           </div>
         </div>
 
-        <div className="mt-4 mx-3 p-4 border border-gray-350 rounded-md flex items-center justify-between shadow-md">
+        <div className="mt-4 mx-3 md:mx-16 p-4 border border-gray-350 rounded-md flex items-center justify-between shadow-md">
           <img
             src={VISA}
-            className="w-[25%] mr-4 bg-transparent"
+            className="w-20 aspect-[3/2] object-contain"
             alt="VISA Logo"
           />
           <div className="text-left">
@@ -55,10 +55,10 @@ const PayMethods = () => {
           </div>
         </div>
 
-        <div className="mt-4 mx-3 p-4 border border-gray-350 rounded-md flex items-center justify-between shadow-md">
+        <div className="mt-4 mx-3 md:mx-16 p-4 border border-gray-350 rounded-md flex items-center justify-between shadow-md">
           <img
             src={PetraStark}
-            className="w-[25%] h-[16%] mr-4 bg-transparent ml-4"
+            className="w-20 aspect-[3/2] object-contain"
             alt="P logo"
           />
           <div className="text-left">
@@ -68,14 +68,14 @@ const PayMethods = () => {
           </div>
         </div>
 
-        <h3 className="transform uppercase font-medium ml-3 mt-10">
+        <h3 className="transform px-3 md:mx-16 uppercase font-medium ml-3 mt-10">
           Current Method
         </h3>
 
-        <div className="mt-4 mx-3 p-4 border border-gray-350 rounded-md flex items-center justify-between relative shadow-md">
+        <div className="mt-4 mx-3 md:mx-16 p-4 border border-gray-350 rounded-md flex items-center justify-between relative shadow-md">
           <img
             src={dollar}
-            className="w-[18%] h-[13%] mr-10 bg-transparent "
+            className="w-20 aspect-[3/2] object-contain"
             alt="dollar Logo"
           />
           <div className="text-left">
@@ -89,10 +89,10 @@ const PayMethods = () => {
           <IoIosArrowDropdownCircle className="text-indigo-900 absolute right-4 top-1/2 transform -translate-y-1/2 text-xl" />
         </div>
 
-        <div className="px-8">
+        <div className="text-center">
           <Link
             to={"/dashboard/@me/addcard"}
-            className="bg-fuchsia-950 w-full mt-16 mb-2 py-2 rounded-lg text-white text-md  transform uppercase"
+            className="bg-fuchsia-950 w-2/3 mt-10 mb-2 py-2 px-5 rounded-lg text-white text-xl font-bold"
           >
             Add Payment Method
           </Link>
