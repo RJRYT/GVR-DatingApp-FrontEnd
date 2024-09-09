5454ae5dc985674ee9684f13d958f743caf44056
RJRYT <robinjratr@gmail.com>
2024-09-09 09:43:13 +0530

    preferences bug fixes & matches ui updated

--- src/components/Matches/MatchGrid/index.jsx
+++ src/components/Matches/MatchGrid/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import MatchCard from "./MatchCard";
+import { Link } from "react-router-dom";
 
 const MatchGrid = ({matches}) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 p-4">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 p-4 pb-20">
       {matches.map((match, index) => (
-        <MatchCard key={index} match={match} />
+        <Link key={index} to={`/dashboard/profile/${match._id}`} >
+        <MatchCard match={match} />
+        </Link>
       ))}
     </div>
   );
