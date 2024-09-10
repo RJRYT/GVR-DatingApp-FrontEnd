49024250f7f120f9b95f270e8bd54c8693e3da7f
RJRYT <robinjratr@gmail.com>
2024-09-10 13:40:35 +0530

    error fix

--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -27,7 +27,7 @@ const UserProfile = ({ upgrade = false }) => {
         } else {
           const response = await axios.get(`/users/profile/${userId}`);
           if (response.data.success) {
-            setUser(response.data.user);
+            setUser({...response.data.user, chatId:response.data.chat});
           }
         }
       } catch (error) {
